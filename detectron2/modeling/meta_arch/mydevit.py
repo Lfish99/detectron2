ed355913b9646e7b388fb339c19e0c977864d855
Lfish99 <94099003+Lfish99@users.noreply.github.com>
2024-01-10 19:57:46 +0800

    Update mydevit.py

--- detectron2/modeling/meta_arch/mydevit.py
+++ detectron2/modeling/meta_arch/mydevit.py
@@ -664,16 +664,16 @@ class DevitNet(nn.Module):
             device = 0
             backbone_model = backbone_model.to(device)
             # 还必须对image进行一下处理，之前的preprocess_image仅仅是detectron2默认的图像处理，就是归一化
-            images = images.permute(1, 2, 0)
+            image = images[0].permute(1, 2, 0)
             resize_op = detectron2.data.transforms.ResizeShortestEdge(
                 short_edge_length=800,
                 max_size=1333,
             )
-            resize = resize_op.get_transform(images)
-            images = torch.as_tensor(resize.apply_image(image.numpy())).permute(2, 0, 1)
+            resize = resize_op.get_transform(image)
+            img = torch.as_tensor(resize.apply_image(image.numpy())).permute(2, 0, 1)
             h, w = img.shape[1:]
             h, w  = max(iround(h / 14), 1) * 14, max(iround(w / 14), 1) * 14
-            images14 = torchvision.transforms.functional.resize(img, (h, w), interpolation=torchvision.transforms.functional.InterpolationMode.BICUBIC)
+            image14 = torchvision.transforms.functional.resize(img, (h, w), interpolation=torchvision.transforms.functional.InterpolationMode.BICUBIC)
             nimage14 = normalize_image(image14)[None, ...]
             r = backbone_model.get_intermediate_layers(nimage14.to(device), return_class_token=True, reshape=True)
             patch_tokens = r[0][0][0].cpu()
