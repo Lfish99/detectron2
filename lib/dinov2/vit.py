0c24d861014993137d066da7eee516008b6ab471
Lfish99 <94099003+Lfish99@users.noreply.github.com>
2024-01-12 00:33:17 +0800

    Update vit.py

--- lib/dinov2/vit.py
+++ lib/dinov2/vit.py
@@ -373,13 +373,13 @@ class DinoVisionTransformer(Backbone):
         return tuple(outputs)
 
     def forward(self, x, is_training=False, **kwargs):
-        print('%%%%%', x)
-        print('####', type(x))
+        # print('%%%%%', x) [[[
+        # print('####', type(x))
         # # from pudb.remote import set_trace; set_trace(term_size=(202, 47), port=12345)  
         # self.enforce_partial_freeze_if_needed()
         # outputs = self.get_intermediate_layers(x, n=self._out_indices, reshape=True)
         mymodel = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14')
-        mymodel = mymodel.to('0')
+        mymodel = mymodel.to('cuda')
         outputs = mymodel.get_intermediate_layers(x, n=self._out_indices, reshape=True)
         return {f'res{i}': o for i, o in zip(self._out_indices, outputs)}
 
